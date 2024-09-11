f89ab25121248bfa257cb943ffff5697f941e890
trunghieudev <dinhletrunghieu0207@gmail.com>
2024-09-11 19:21:57 +0700

    fix: responsive brain aniamtion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Brain } from "threejs-brain-animation";
 import "threejs-brain-animation/dist/main.css";
 
 export default function Example() {
-  const width = 1000;
-  const height = 500;
+  const [dimensions, setDimensions] = useState({ width: 600, height: 500 });
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = Math.min(window.innerWidth * 0.9, 800);
+      const height = (width / 16) * 9;
+      setDimensions({ width, height });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Brain width={width} height={height} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Brain width={dimensions.width} height={dimensions.height} />
     </div>
   );
 }
